97dd30c5c2c34fafd11a09a72eb75465e7ddf99c
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-18 13:33:29 +0000

    Ajout des donnees lies au TalentType et son migration

--- database/migrations/2024_03_18_133212_create_talent_table.php
+++ database/migrations/2024_03_18_133212_create_talent_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('talent', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('talent_type_id')->constrained('talent_types');
             $table->timestamps();
         });
     }
