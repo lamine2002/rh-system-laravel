52a3e2c44efb4534b1d3a2632882b07f78c6031f
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-03-20 21:09:35 +0000

    implementation du controller de absence

--- app/Http/Controllers/AbsenceController.php
+++ app/Http/Controllers/AbsenceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AbsenceFormRequest;
 use App\Models\Absence;
 use Illuminate\Http\Request;
 
@@ -21,6 +22,35 @@ class AbsenceController extends Controller
     public function create()
     {
         return view('absences.create');
+    }
+
+    public function store(AbsenceFormRequest $request)
+    {
+        Absence::create($request->validated());
+        return redirect()->route('absences.index');
+    }
+
+//    public function show(Absence $absence)
+//    {
+//        return view('absences.show', compact('absence'));
+//    }
+
+
+    public function edit(Absence $absence)
+    {
+        return view('absences.edit', compact('absence'));
+    }
+
+    public function update(AbsenceFormRequest $request, Absence $absence)
+    {
+        $absence->update($request->validated());
+        return redirect()->route('absences.index');
+    }
+
+    public function destroy(Absence $absence)
+    {
+        $absence->delete();
+        return redirect()->route('absences.index');
     }
 
 
