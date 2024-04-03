7d0dd13958d8b4866e510caa24016b095c35c5c4
Niang Mohamed Lamine <106320478+lamine2002@users.noreply.github.com>
2024-04-03 00:26:24 +0000

    Ajout de popup dans les alertes mails

--- resources/views/rh/mail-alerts/index.blade.php
+++ resources/views/rh/mail-alerts/index.blade.php
@@ -45,13 +45,23 @@
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="text-sm font-medium text-gray-900 dark:text-white">
-                                {{ $mail_alert->message }}
+                                {{ substr($mail_alert->message, 0, 25 ).' ...' }}<button data-popover-target="popover-bottom{{$mail_alert->id}}" data-popover-placement="bottom" type="button" ><svg class="w-4 h-4 ms-2 text-gray-400 hover:text-gray-500" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path></svg><span class="sr-only">Show information</span></button>
+
                             </div>
                         </td>
                         <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                             <a href="{{ route('rh.mail-alerts.edit', $mail_alert) }}" class="text-indigo-600 hover:text-indigo-900">Modifier</a>
                         </td>
                     </tr>
+                    <div data-popover id="popover-bottom{{$mail_alert->id}}" role="tooltip" class="absolute z-10 invisible inline-block w-64 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm opacity-0 dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800">
+                        <div class="px-3 py-2 bg-gray-100 border-b border-gray-200 rounded-t-lg dark:border-gray-600 dark:bg-gray-700">
+                            <h3 class="font-semibold text-gray-900 dark:text-white">Message</h3>
+                        </div>
+                        <div class="px-3 py-2">
+                            <p>{{ $mail_alert->message }}</p>
+                        </div>
+                        <div data-popper-arrow></div>
+                    </div>
                     @endforeach
                     </tbody>
                 </table>
@@ -59,5 +69,6 @@
         </div>
     </div>
 </div>
+
 
 @endsection
